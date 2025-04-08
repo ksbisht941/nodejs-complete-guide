3c56322e5b7ad9fb62c3c2c3ce8f866c78887138
ksbisht <ksbisht941@gmail.com>
2025-04-08 20:53:14 +0530

    update project

--- app.js
+++ app.js
@@ -1,30 +1,56 @@
-const http = require('http');
-const express = require('express');
 const path = require('path');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(express.urlencoded({ extended: false }));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
-app.use('/', shopRoutes);
+app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-// Create server
-const server = http.createServer(app);
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
 
-// Start server
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+sequelize
+  // .sync({ force: true })
+  .sync()
+  .then(result => {
+    return User.findByPk(1);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Max', email: 'test@test.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,24 @@
+const path = require('path');
+
 const express = require('express');
-// const path = require('path');
-// const rootDir = require('../util/path');
+
 const adminController = require('../controllers/admin');
+
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get('/add-product', adminController.getAddProduct);
 
-// /admin/add-product => POST
-router.post('/product', adminController.postAddProduct);
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
-// exports.routes = router;
-// exports.products = products;
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
+
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product', adminController.postEditProduct);
+
+router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;
